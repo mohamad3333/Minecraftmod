93594ee8916b4b4d7c5f6927a1d9b3a52d1150cd
Mohammad mahamid <mohamad333333333@hotmail.com>
2015-10-27 21:01:31 +0100

    Tutorial 5

--- src/apple.java
+++ src/apple.java
@@ -1,8 +1,13 @@
 
 public class apple {
-	
-public static void main(String args[]) {
-	System.out.println("Hello world!");
+	public static void main(String args[]){
+		double tuna;
+		tuna= 5.66;
+		
+		System.out.print("i want ");
+		System.out.print(tuna);
+		System.out.println(" movies");
+		System.out.println("movies");
+	}
 
-}
 }
