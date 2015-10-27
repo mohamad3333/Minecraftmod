b16e8d0d8077d8e05379bdf83b8e17dc597e5e47
Mohammad mahamid <mohamad333333333@hotmail.com>
2015-10-27 20:37:19 +0100

    first push

---
+++ Mohammad program/src/Mohammad/program/Hello.java
@@ -0,0 +1,9 @@
+package Mohammad.program;
+
+public class Hello {
+		// main: generate some simple output
+		public static void main(String[] args) {
+		System.out.println("Hello, world.");
+		}
+
+}
