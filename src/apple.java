f40d8eb4df76cbc2fc4534a8051980ce0b5e860f
mohamad <mohamad@aaa.localdomain>
2015-10-28 18:52:46 +0100

    tutorial 8

--- src/apple.java
+++ src/apple.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class apple {
 	public static void main(String args[]){
 		Scanner bucky = new Scanner(System.in);
-        System.out.println(bucky.nextLine()); //allow you to pause and wait for input
+        int x,y,z;
+        x= 11;
+        y= 3;
+        z=x%y;
+        System.out.println(z); //allow you to pause and wait for input
 	}
-
 }
