a5d73608ba0f6841aa41d0cb511cb452a8a81424
Mohammad mahamid <mohamad333333333@hotmail.com>
2015-10-28 17:01:43 +0100

    added comment

--- src/apple.java
+++ src/apple.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class apple {
 	public static void main(String args[]){
 		Scanner bucky = new Scanner(System.in);
-        System.out.println(bucky.nextLine());
+        System.out.println(bucky.nextLine()); //allow you to pause and wait for input
 	}
 
 }
